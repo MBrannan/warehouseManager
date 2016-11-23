6218a9b661e6fb6ab1ad07bb22a08961a2c87d38
Joseph Giwoyna <josephgiwoyna@Josephs-MacBook-Pro-2.local>
2016-11-23 10:25:05 -0600

    added client.js and templates

---
+++ server/public/scripts/client.js
@@ -0,0 +1,57 @@
+var app = angular.module('myApp', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/home', {
+      templateUrl: '/views/templates/home.html',
+      controller: 'HomeController',
+      controllerAs: 'home'
+    })
+    .when('/customers' ,{
+      templateUrl: '/views/templates/customers.html',
+      controller: 'CustomersController',
+      controllerAs: 'customers'
+    })
+    .when('/orders' ,{
+      templateUrl: '/views/templates/orders.html',
+      controller: 'OrdersController',
+      controllerAs: 'orders'
+    })
+    .when('/warehouse' ,{
+      templateUrl: '/views/templates/warehouse.html',
+      controller: 'WarehouseController',
+      controllerAs: 'warehouse'
+    })
+    .otherwise({
+      redirectTo: 'home'
+    });
+
+}]);
+
+app.controller('HomeController', function() {
+  console.log('home controller running');
+  var self = this;
+  self.message = "Home controller is the best!";
+
+});
+
+app.controller('CustomersController', function() {
+  console.log('Customers controller running');
+  var self = this;
+  self.message = "Customers controller is the best!";
+
+});
+
+app.controller('OrdersController', function() {
+  console.log('Orders controller running');
+  var self = this;
+  self.message = "Orders controller is the best!";
+
+});
+
+app.controller('WarehouseController', function() {
+  console.log('Warehouse controller running');
+  var self = this;
+  self.message = "Warehouse controller is the best!";
+
+});
